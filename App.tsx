ee101bc805164fd783e2b227841a8c4d9abeccb4
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-05-05 13:58:24 -0300

    Styled Container, Title and Form component.

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import Form from "./src/components/Form/Index";
+import Title from "./src/components/Title/Index";
+import * as C from "./styles";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<>
+			<C.Container>
+				<Title />
+				<Form />
+			</C.Container>
+		</>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
