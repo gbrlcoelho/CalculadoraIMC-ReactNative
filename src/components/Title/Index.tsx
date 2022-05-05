1f83821e26007e6fc26431920b32a4d69d5447c6
Gabriel Coelho <coelhogabriel67@gmail.com>
2022-05-05 13:59:59 -0300

    Title Component

---
+++ src/components/Title/Index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import * as C from "../../../styles";
+
+export default function Title() {
+	return (
+		<C.BoxTitle>
+			<C.TextTitle>CALCULADORA IMC</C.TextTitle>
+		</C.BoxTitle>
+	);
+}
